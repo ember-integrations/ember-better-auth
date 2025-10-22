5b6aaa8373219f4cba194ec4777e5ce211e2d024
Thomas Gossmann <gossi@users.noreply.github.com>
2025-10-22 16:25:48 +0200

    Fix CI (#2)

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,38 +1,38 @@
-import { Hono } from "hono";
-import { auth } from "./auth"; // path to your auth file
-import { serve } from "@hono/node-server";
-import { cors } from "hono/cors";
+import { serve } from '@hono/node-server';
+import { Hono } from 'hono';
+import { cors } from 'hono/cors';
 import { logger } from 'hono/logger';
+
+import { auth } from './auth'; // path to your auth file
 
 const app = new Hono();
 
-app.use(logger())
-// app.use('/api/auth/*', cors());
+app.use(logger());
 
 app.use(
-	"/api/auth/*", // or replace with "*" to enable cors for all routes
-	cors({
-		origin: "http://localhost:4200", // replace with your origin
-    allowHeaders: ["Content-Type", "Authorization"],
-		allowMethods: ["POST", "GET", "OPTIONS"],
-		exposeHeaders: ["Content-Length"],
-		maxAge: 600,
-		credentials: true,
-	}),
+  '/api/auth/*', // or replace with "*" to enable cors for all routes
+  cors({
+    origin: 'http://localhost:4200', // replace with your origin
+    allowHeaders: ['Content-Type', 'Authorization'],
+    allowMethods: ['POST', 'GET', 'OPTIONS'],
+    exposeHeaders: ['Content-Length'],
+    maxAge: 600,
+    credentials: true
+  })
 );
 
-
 // Handle preflight OPTIONS requests for CORS
-app.options("/api/auth/*", (c) => {
-	return c.body(null, 204);
+app.options('/api/auth/*', (c) => {
+  // eslint-disable-next-line unicorn/no-null
+  return c.body(null, 204);
 });
 
-app.on(["POST", "GET"], "/api/auth/*", async (c) => {
-	const handle = await auth.handler(c.req.raw)
+app.on(['POST', 'GET'], '/api/auth/*', async (c) => {
+  const handle = await auth.handler(c.req.raw);
 
-	console.log(handle);
+  console.log(handle);
 
-	return handle;
+  return handle;
 });
 
 serve(app);
--- web/src/services/auth.ts
+++ web/src/services/auth.ts
@@ -3,3 +3,9 @@ import { AuthService as UpstreamAuthService } from 'ember-better-auth';
 import type { User } from '../domain/user';
 
 export default class AuthService extends UpstreamAuthService<User> {}
+
+declare module '@ember/service' {
+  interface Registry {
+    auth: AuthService;
+  }
+}
