f68b2c54e8670884822db4cc4b455881dd41801d
gossi <gossi@users.noreply.github.com>
2025-10-22 16:03:50 +0200

    fix lint

--- server/src/auth.ts
+++ server/src/auth.ts
@@ -1,16 +1,13 @@
-import { betterAuth } from "better-auth";
-import { openAPI } from "better-auth/plugins"
-import Database from "better-sqlite3";
+import { betterAuth } from 'better-auth';
+import { openAPI } from 'better-auth/plugins';
+// eslint-disable-next-line import-x/no-named-as-default
+import Database from 'better-sqlite3';
 
 export const auth = betterAuth({
-  database: new Database("./sqlite.db"),
-  trustedOrigins: [
-    'http://localhost:4200'
-  ],
+  database: new Database('./sqlite.db'),
+  trustedOrigins: ['http://localhost:4200'],
   emailAndPassword: {
     enabled: true
   },
-  plugins: [
-    openAPI(),
-  ]
-})
+  plugins: [openAPI()]
+});
